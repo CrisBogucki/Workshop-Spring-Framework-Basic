ed149be74850ad6289876997d7d63edd95d860a9
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2017-07-24 15:03:44 +0200

    Prezentacja konfuguracji za pomocą XML

--- src/main/java/pl/asseco/workshop/calculationModule/CalculationModuleApplication.java
+++ src/main/java/pl/asseco/workshop/calculationModule/CalculationModuleApplication.java
@@ -3,6 +3,7 @@ package pl.asseco.workshop.calculationModule;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.asseco.workshop.calculationModule.Config.AppConfig;
 import pl.asseco.workshop.calculationModule.Services.AdderServiceOne;
 import pl.asseco.workshop.calculationModule.Services.AdderServiceTwo;
@@ -13,7 +14,7 @@ public class CalculationModuleApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(CalculationModuleApplication.class, args);
 
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("/AppConfig.xml");
 
 		AdderServiceOne service = context.getBean(AdderServiceOne.class);
 	 	service.sum();
